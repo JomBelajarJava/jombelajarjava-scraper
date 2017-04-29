0b1a67ca3f24a3b9d212b20aff45e6169f1ef92b
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-04-29 19:05:47 +0800

    Minor refactor

--- scrapper.lisp
+++ scrapper.lisp
@@ -5,8 +5,8 @@
 
 (defun sub-menu-p (node)
   (and (typep node 'stp:element)
-       (string= (stp:local-name node) "ul")
-       (string= (stp:attribute-value node "class") "sub-menu")))
+       (equal (stp:local-name node) "ul")
+       (equal (stp:attribute-value node "class") "sub-menu")))
 
 (defun parse-link (node)
   (stp:attribute-value (stp:first-child node) "href"))
@@ -14,6 +14,6 @@
 (defun parse-links-in-menu ()
   (let* ((homepage  (drakma:http-request "http://www.jombelajarjava.com"))
          (document  (chtml:parse homepage (stp:make-builder)))
-         (menu-list (alexandria:flatten (mapcar #'stp:list-children
-                                                (stp:filter-recursively #'sub-menu-p document)))))
+         (sub-menus (stp:filter-recursively #'sub-menu-p document))
+         (menu-list (alexandria:flatten (mapcar #'stp:list-children sub-menus))))
     (mapcar #'parse-link menu-list)))
