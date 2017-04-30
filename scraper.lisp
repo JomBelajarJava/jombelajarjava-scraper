c31158232e536dfeff88181476a13af58434fd02
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-04-30 09:51:12 +0800

    Add content scraping

--- scraper.lisp
+++ scraper.lisp
@@ -3,6 +3,8 @@
 (ql:quickload :closure-html)
 (ql:quickload :cxml-stp)
 
+(defparameter *base-url* "http://www.jombelajarjava.com")
+
 (defun sub-menu-p (node)
   (and (typep node 'stp:element)
        (equal (stp:local-name node) "ul")
@@ -12,8 +14,28 @@
   (stp:attribute-value (stp:first-child node) "href"))
 
 (defun parse-links-in-menu ()
-  (let* ((homepage  (drakma:http-request "http://www.jombelajarjava.com"))
+  (let* ((homepage  (drakma:http-request *base-url*))
          (document  (chtml:parse homepage (stp:make-builder)))
          (sub-menus (stp:filter-recursively #'sub-menu-p document))
          (menu-list (alexandria:flatten (mapcar #'stp:list-children sub-menus))))
     (mapcar #'parse-link menu-list)))
+
+(defun contentp (node)
+  (and (typep node 'stp:element)
+       (equal (stp:local-name node) "div")
+       (equal (stp:attribute-value node "class") "entry-content")))
+
+(defun scrape-content (link)
+  (let* ((filename (remove #\/ (subseq link (length *base-url*))))
+         (page     (drakma:http-request link))
+         (document (chtml:parse page (stp:make-builder)))
+         (content  (first (stp:filter-recursively #'contentp document))))
+    (with-open-file (*output-file* (ensure-directories-exist
+                                    (format nil "out/~a.html" filename))
+                                   :direction :output
+                                   :if-exists :supersede)
+      (stp:serialize content (chtml:make-character-stream-sink *output-file*)))))
+
+(defun scrape-all-content ()
+  (dolist (link (parse-links-in-menu))
+    (scrape-content link)))
